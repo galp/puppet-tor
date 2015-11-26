c48feb55ac573c02a5cf0f2d9951059be8ac4171
ag <ag@kiben.net>
2015-11-26 20:51:19 +0000

    basic support for relays

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,12 @@
 class tor (
   $gateway               = false,
   $hidden_service        = true,
+  $tor_relay             = false,
+  $relay_contact_info    = '0xFFFFFFFF Random Person <nobody AT example dot com>',
+  $nickname              = 'sillynickname',
+  $orport                = '9001',
+  $bandwidth_rate        = '100',
+  $bandwidth_burst       = '200',
   $hidden_service_dir    = 'default',
   $hidden_service_ports  = 'default',
   $basename              = 'lol',
